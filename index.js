24894176d1171643b6f70e6cb704597f72a753d2
ShakirJ2325 <66296342+ShakirJ2325@users.noreply.github.com>
2025-07-15 02:14:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,5 +29,9 @@ app.get("/api/reports", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
 app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
